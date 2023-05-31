866f46aa70ba3d9cbf783d839e7e80010a334eca
TheIceJi <Jirayu.ninl@gmail.com>
2023-05-31 22:03:36 +0700

    [Improve] Structure and files

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -1,5 +1,5 @@
 import * as Sentry from '@sentry/nextjs'
-import config from '@app/config'
+import config from '@global/config'
 
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN
 
