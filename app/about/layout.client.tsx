a015db5653383cd9f868620a77e16ba9be2804d5
TheIceJi <Jirayu.ninl@gmail.com>
2023-08-08 04:18:14 +0700

    [FIX] Hydration fail

--- app/about/layout.client.tsx
+++ app/about/layout.client.tsx
@@ -1,10 +1,9 @@
 'use client'
 
-import { Suspense } from 'react'
-import { Loader, OrthographicCamera } from '@react-three/drei'
-import { Canvas } from '@react-three/fiber'
-import Background from './components/scene'
-import { UI } from '@global/store'
+import dynamic from 'next/dynamic'
+const Scene = dynamic(() =>
+  import('./layout.scene').then((module) => module.Scene),
+)
 
 export default function SceneLayout({
   children,
@@ -14,31 +13,7 @@ export default function SceneLayout({
   return (
     <div className='m-container w-svw relative flex overflow-hidden'>
       <div className='absolute z-10 flex h-full w-full'>{children}</div>
-      <Scene />
-    </div>
-  )
-}
-
-const Scene = () => {
-  const _dark = UI((state) => state.dark)
-  return (
-    <div className='absolute h-full w-full overflow-hidden'>
-      <Canvas
-        gl={{
-          powerPreference: 'high-performance',
-          alpha: true,
-          antialias: true,
-          stencil: false,
-          depth: false,
-        }}
-        linear={true}
-      >
-        <Suspense fallback={null}>
-          <OrthographicCamera makeDefault position={[0, 0, 0]} zoom={230} />
-          <Background _dark={_dark} />
-        </Suspense>
-      </Canvas>
-      <Loader />
+      {Scene && <Scene />}
     </div>
   )
 }
